af7596f296a7c6809b3392b7ede5065a58c0a6d7
smoke <smoke571reg@yandex.ru>
2020-05-13 13:44:54 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+let isDevEnv = (process.env.NODE_ENV === "development");
+
+module.exports = {
+    mode: isDevEnv ? "development" : "production",
+    entry: "./src/index.js",
+
+    output: {
+        filename: "[name].[contenthash].js",
+        chunkFilename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: true }),
+        new HtmlWebpackPlugin({
+            title: "Site",
+            template: "./src/index.html"
+        })
+    ],
+
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        historyApiFallback: true,
+        writeToDisk: true
+    },
+
+    devtool: isDevEnv ? "inline-source-map" : undefined,
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+};
