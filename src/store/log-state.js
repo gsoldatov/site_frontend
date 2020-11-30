68715a6145448cd90b263a77df91480e9ad667e3
smoke <smoke571reg@yandex.ru>
2020-11-30 14:04:26 +0300

    Fixed log state errors caused by treating nulls the same way as objects.

--- src/store/log-state.js
+++ src/store/log-state.js
@@ -2,50 +2,51 @@ const LOGGING_ORDER = ["tagUI", "tagsUI", "objectUI", "objectsUI", "tags", "obje
 
 
 export default (state, topLevelHeader) => {
-   /*
-        Function for formatted logging of the state into the console.
-        Prints top-level parts of the state set in the LOGGING_ORDER list, then prints the rest of the state.
-    */
-    const log = (state, header, isTopLevel = false) => {
-        let oneLine = true;
-        if (typeof(state) === "object") {
-            if (state instanceof Array) {
-                for (let item of state) {
-                    if (typeof(item) === "object") {
-                        oneLine = false;
-                        break;
-                    }
-                }
-            } else {
-                for (let item of Object.values(state)) {
-                    if (typeof(item) === "object") {
-                        oneLine = false;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (oneLine) {
-            console.log(`${header}: ${JSON.stringify(state)}`);
-        } else {
-            console.groupCollapsed(header);
-            if (typeof(state) === "object") {
-                if (state instanceof Array) {
-                    for (let item in state) {
-                        log(state[item], item);
-                    }
-                } else {
-                    let keys = isTopLevel ? topLeveLoggingOrder : Object.keys(state);
-                    for (let key of keys) {
-                        log(state[key], key);
-                    }
-                }
-            }
-            console.groupEnd();
-        }
-    }
-
-    let topLeveLoggingOrder = [...LOGGING_ORDER, ...Object.keys(state).filter(k => LOGGING_ORDER.indexOf(k) === -1)];
-    log(state, topLevelHeader, true);
-};
+    /*
+         Function for formatted logging of the state into the console.
+         Prints top-level parts of the state set in the LOGGING_ORDER list, then prints the rest of the state.
+     */
+     const log = (state, header, isTopLevel = false) => {
+         let oneLine = true;
+         if (typeof(state) === "object" && state !== null) {
+             if (state instanceof Array) {
+                 for (let item of state) {
+                     if (typeof(item) === "object") {
+                         oneLine = false;
+                         break;
+                     }
+                 }
+             } else {
+                 for (let item of Object.values(state)) {
+                     if (typeof(item) === "object" && item !== null) {
+                         oneLine = false;
+                         break;
+                     }
+                 }
+             }
+         }
+ 
+         if (oneLine) {
+             console.log(`${header}: ${JSON.stringify(state)}`);
+         } else {
+             console.groupCollapsed(header);
+             if (typeof(state) === "object") {
+                 if (state instanceof Array) {
+                     for (let item in state) {
+                         log(state[item], item);
+                     }
+                 } else {
+                     let keys = isTopLevel ? topLeveLoggingOrder : Object.keys(state);
+                     for (let key of keys) {
+                         log(state[key], key);
+                     }
+                 }
+             }
+             console.groupEnd();
+         }
+     }
+ 
+     let topLeveLoggingOrder = [...LOGGING_ORDER, ...Object.keys(state).filter(k => LOGGING_ORDER.indexOf(k) === -1)];
+     log(state, topLevelHeader, true);
+ };
+ 
