1a27f057f507ad4d575889814d22223042a65de1
smoke <smoke571reg@yandex.ru>
2021-07-14 21:43:30 +0400

    Fixed pagination display on small screens.

--- src/components/field/field-pagination.js
+++ src/components/field/field-pagination.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import { Pagination } from "semantic-ui-react";
+
+import { OnResizeWrapper } from "../common/on-resize-wrapper";
 
 import StyleFieldPagination from "../../styles/field-pagination.css";
 
@@ -15,12 +16,23 @@ export default ({ paginationInfoSelector, setCurrentPage }) => {
     const currentPage = paginationInfo ? paginationInfo.currentPage : null;
     const totalPages = paginationInfo ? Math.ceil(paginationInfo.totalItems / paginationInfo.itemsPerPage) : null;
     
-    const onChange = (e, props) => {
+    const onChange = useMemo(() => (e, props) => {
         dispatch(setCurrentPage(props.activePage));
-    }
+    });
+
+    // Change pagination parameters based on viewport width
+    const [isFullscreenStyle, setIsFullscreenStyle] = useState(window.innerWidth >= 500);
+    const onResizeCallback = useMemo(() => computedStyle => {
+        const width = parseInt(computedStyle.width.replace("px", ""));
+        setIsFullscreenStyle(width >= 500);
+    });
+    const siblingRange = isFullscreenStyle ? 2 : 0;
+
     return paginationInfo && totalPages > 1 && (
-        <div className="field-pagination">
-            <Pagination activePage={currentPage} totalPages={totalPages} siblingRange={2} firstItem={null} lastItem={null} onPageChange={onChange}/>
-        </div>
+        <OnResizeWrapper callback={onResizeCallback}>
+            <div className="field-pagination-container">
+                <Pagination className="field-pagination" activePage={currentPage} totalPages={totalPages} siblingRange={siblingRange} firstItem={null} lastItem={null} onPageChange={onChange}/>
+            </div>
+        </OnResizeWrapper>
     );
 };
