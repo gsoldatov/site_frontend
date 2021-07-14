bf3fc117fa5f689dbfd21c16775bc2dd478b8fbe
smoke <smoke571reg@yandex.ru>
2021-07-14 14:39:55 +0400

    Updated nagivation bar style.

--- src/components/common/navigation.js
+++ src/components/common/navigation.js
@@ -3,6 +3,8 @@ import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
+import StyleNavigation from "../../styles/navigation.css";
+
 
 /**
  * Navigation bar.
@@ -10,7 +12,9 @@ import { useLocation } from "react-router-dom";
 export default () => {
     const location = useLocation();
     return (
-        <Menu inverted activeIndex={location.pathname}>
+        <Menu inverted fluid size="huge" className="navigation-bar"
+            activeIndex={location.pathname}
+        >
             <Menu.Item as={NavLink} exact to="/">Index</Menu.Item>
             <Menu.Item as={NavLink} exact to="/objects">Objects</Menu.Item>
             <Menu.Item as={NavLink} exact to="/tags">Tags</Menu.Item>
