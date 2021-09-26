b3b12902ac6e10a0118933a09a4652cb2603c53b
smoke <smoke571reg@yandex.ru>
2021-09-26 21:10:08 +0300

    Moved navigation bar's items and secondary menu with auth controls into separate components.

--- src/components/common/navigation.js
+++ src/components/common/navigation.js
@@ -1,6 +1,6 @@
 import React, { memo, useEffect, useMemo, useState } from "react";
 import { Button, Icon, Label, Menu } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 import { OnResizeWrapper } from "./on-resize-wrapper";
@@ -16,8 +16,8 @@ const navigationItems = [
     { to: "/", text: "Index" },
     { to: "/objects", text: "Objects" },
     { to: "/objects/edited", text: "Edited Objects", 
-        getLabelText: state => Object.keys(state.editedObjects).length || undefined, 
-        getLabelColor: state => Object.keys(state.editedObjects).length > 0 ? "green" : "grey" },
+        labelTextSelector: state => Object.keys(state.editedObjects).length || undefined, 
+        labelColorSelector: state => Object.keys(state.editedObjects).length > 0 ? "green" : "grey" },
     { to: "/tags", text: "Tags" }
 ]
 
@@ -52,39 +52,11 @@ export default memo(({ itemOnClickCallback }) => {
         isStacked ? " is-stacked" : ""  // add a top-border when menu is vertical (instead of pseudo-elements displayed before other menu items)
     );
 
-    const itemProps = navigationItems.map((item, k) => {    // Always run all useSelector hooks for menu items
-        const props = {};
-        if (item.getLabelText) props.labelText = useSelector(item.getLabelText);
-        if (item.getLabelColor) props.labelColor = useSelector(item.getLabelColor);
-        return props;
-    });
-
     let menuItems;
     if (!isStacked || isExpanded) {
-        menuItems = navigationItems.map((item, k) => {
-            let label = null;
-            if (item.getLabelText) {
-                const labelText = itemProps[k].labelText;
-                const labelColor = itemProps[k].labelColor || "grey";
-                if (labelText !== undefined) label = <Label size="tiny" circular color={labelColor}>{labelText}</Label>;
-            }
+        menuItems = navigationItems.map((item, k) => <NavbarItem key={k} item={item} itemOnClickCallback={itemOnClickCallback} />);
 
-            return (
-                <Menu.Item key={k} as={NavLink} exact to={item.to} onClick={itemOnClickCallback}>
-                    {item.text}
-                    {label}
-                </Menu.Item>
-            );
-        });
-
-        menuItems.push(
-            <Menu.Menu key={-1} position="right" className={rightMenuClassName}>
-                <Menu.Item className="nagivation-bar-button-container">
-                    <Button className="navigation-bar-button" color="blue">Login</Button>
-                    <Button className="navigation-bar-button" color="teal">Sign Up</Button>
-                </Menu.Item>
-            </Menu.Menu>
-        );
+        menuItems.push(<AuthSubMenu key={-1} containerClassName={rightMenuClassName} />);
     }
     
     // `vertical` <Menu> prop is used instead of `stackable` prop of <Menu> to avoid inconsistency when when displaying composite object page
@@ -100,3 +72,43 @@ export default memo(({ itemOnClickCallback }) => {
         </OnResizeWrapper>
     );
 });
+
+
+/**
+ * Navigation bar's item component.
+ */
+const NavbarItem = ({ item, itemOnClickCallback }) => {
+    const isDisplayed = useSelector(item.isDisplayedSelector || (state => true));
+    const labelText = useSelector(item.labelTextSelector || (state => undefined));
+    const labelColor = useSelector(item.labelColorSelector || (state => "grey"));
+
+    if (!isDisplayed) return null;
+
+    const label = labelText !== undefined ? <Label size="tiny" circular color={labelColor}>{labelText}</Label> : null;
+
+    return (
+        <Menu.Item as={NavLink} exact to={item.to} onClick={itemOnClickCallback}>
+            {item.text}
+            {label}
+        </Menu.Item>
+    );
+};
+
+
+/**
+ * Navigation bar's secondary menu component with auth controls.
+ */
+const AuthSubMenu = ({ containerClassName }) => {
+    const history = useHistory();
+
+    const loginOnClick = useMemo(() => () => history.push("/auth/login"));
+
+    return (
+        <Menu.Menu position="right" className={containerClassName}>
+            <Menu.Item className="nagivation-bar-button-container">
+                <Button className="navigation-bar-button" color="blue" onClick={loginOnClick}>Login</Button>
+                <Button className="navigation-bar-button" color="teal">Sign Up</Button>
+            </Menu.Item>
+        </Menu.Menu>
+    );
+};
