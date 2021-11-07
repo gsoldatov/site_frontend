df3cb2ea3b2ea54493fbf06ccee673161ad4e2cb
smoke <smoke571reg@yandex.ru>
2021-11-07 14:38:02 +0300

    Fixed errors on the /objects/view/:id page caused by state reset after logout.

--- src/components/view/object-attributes/object-attributes.js
+++ src/components/view/object-attributes/object-attributes.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import { ObjectTimeStamp } from "./object-timestamp";
 import { ObjectDescription } from "./object-description";
@@ -9,7 +10,10 @@ import { ObjectHeader } from "./object-header";
  * Container for object/subobject attributes.
  */
 export const ObjectAttributes = ({ objectID }) => {
-    return (
+    // Don't render if object attributes are not present in the local state (to avoid errors after logout)
+    const canRender = useSelector(state => state.objects[objectID] !== undefined);
+
+    return canRender && (
         <div className="objects-view-attributes">
             <ObjectTimeStamp objectID={objectID} />
             <ObjectHeader objectID={objectID} />
--- src/components/view/object-data/object-data-switch.js
+++ src/components/view/object-data/object-data-switch.js
@@ -9,7 +9,11 @@ import { ObjectDataToDoList } from "./object-data-to-do-list";
  * Switch component for object data on the /objects/view/:id page, based on object type and display properties.
  */
 export const ObjectDataSwitch = ({ objectID }) => {
-    const objectType = useSelector(state => state.objects[objectID].object_type);
+    const canRender = useSelector(state => state.objects[objectID] !== undefined);
+    const objectType = useSelector(state => (state.objects[objectID] || {}).object_type);
+
+    // Don't render if object attributes are not present in the local state (to avoid errors after logout)
+    if (!canRender) return null;
 
     switch (objectType) {
         case "link":
