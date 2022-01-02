f60cff3be3bbca39ffa60821c2924327b174fcd1
smoke <smoke571reg@yandex.ru>
2022-01-02 22:56:57 +0400

    Added CSS classnames to layout grid and its elements.

--- src/components/common/layout.js
+++ src/components/common/layout.js
@@ -13,16 +13,26 @@ export default ({ sideMenuItems, body, className }) => {
     // Side menu column (hide if it's stacked & no `sideMenuItems` are provided)
     const [isStacked, setIsStacked] = useState(window.innerWidth < 768);
     const onResizeCallback = useMemo(() => gridRef => {
-        const width = parseInt(getComputedStyle(gridRef).width.replace("px", ""));
-        setIsStacked(width < 768);     // 768 = SUIR @media threshold
+        // const width = parseInt(getComputedStyle(gridRef).width.replace("px", ""));
+        // setIsStacked(width < 768);     // 768 = SUIR @media threshold
+
+        setIsStacked(window.innerWidth < 768);   // 768 = SUIR @media threshold
     });
     const showSideMenuColumn = !isStacked || sideMenuItems;
+
+    // Grid classnames
+    const customClassNamePostfix = (className ? ` ${className}` : "");
+    const gridClassName = "layout-grid" + customClassNamePostfix;
+    const navigationRowClassName = "layout-grid-navigation-row" + customClassNamePostfix;
+    const mainRowClassName = "layout-grid-main-row" + customClassNamePostfix;
+    const sideMenuColumnClassName = "layout-grid-side-menu-column" + customClassNamePostfix;
+    const mainContentColumnClassName = "layout-grid-main-content-column" + customClassNamePostfix;
     
-    // Side menu column classname (add an additional classname if side menu is stacked)
-    let sideMenuColumnClassName = isStacked ? "stacked-side-menu-column" : "";
-    sideMenuColumnClassName += className 
-        ? (sideMenuColumnClassName.length > 0 ? " " : "") + className
-        : "";
+    // // Side menu column classname (add an additional classname if side menu is stacked)
+    // let sideMenuColumnClassName = isStacked ? "stacked-side-menu-column" : "";
+    // sideMenuColumnClassName += className 
+    //     ? (sideMenuColumnClassName.length > 0 ? " " : "") + className
+    //     : "";
     const sideMenuColumn = showSideMenuColumn && (
         <Grid.Column width={2} className={sideMenuColumnClassName}>
             <SideMenu items={sideMenuItems} />
@@ -33,13 +43,13 @@ export default ({ sideMenuItems, body, className }) => {
 
     return (
         <OnResizeWrapper callback={onResizeCallback}>
-            <Grid stackable className={className}>
-                <Grid.Row className={className}>
+            <Grid stackable className={gridClassName}>
+                <Grid.Row className={navigationRowClassName}>
                     <Navigation />
                 </Grid.Row>
-                <Grid.Row columns={mainRowColumns} className={className}>
+                <Grid.Row columns={mainRowColumns} className={mainRowClassName}>
                     {sideMenuColumn}
-                    <GridColumn width={mainColumnWidth} className={className}>
+                    <GridColumn width={mainColumnWidth} className={mainContentColumnClassName}>
                         {body}
                     </GridColumn>
                 </Grid.Row>
