4f3498ca6d12055287c4148c380e00aaf53fd19a
smoke <smoke571reg@yandex.ru>
2022-05-22 14:41:25 +0300

    Removed comment about locale setting.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { WrappedApp }from "./components/top-level/app";
+
 import StyleRoot from "./styles/root.css"
 
-// TODO get preferred locale and set it for moment before rendering the app
-// import moment from "moment";
-// import "moment/locale/ru";
-// moment.updateLocale("ru");
 
 ReactDOM.render(<WrappedApp />, document.getElementById("root"));
