95c60ce6cf3b21c0260b8bbcab2ad9a21163b340
smoke <smoke571reg@yandex.ru>
2022-05-23 18:52:48 +0300

    Fixed resetConfig function not properly resetting app config.

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ import originalConfig from "./config.json";
 
 export let config = deepCopy(originalConfig.app);
 export const setConfig = newConfig => { config = newConfig; };
-export const resetConfig = () => { config = deepCopy(originalConfig); };
+export const resetConfig = () => { config = deepCopy(originalConfig.app); };
