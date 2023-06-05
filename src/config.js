a7a9cce79a603d29e93785f94d4c43d1f43f8c99
-- <smoke571reg@yandex.ru>
2023-06-05 15:03:47 +0300

    Moved app config into document.app

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ import { deepCopy } from "./util/copy";
 
 import originalConfig from "./config.json";
 
+if (!document.app) document.app = {};
+document.app.config = deepCopy(originalConfig.app);
 
-document.appConfig = deepCopy(originalConfig.app);
-
-export const getConfig = () => document.appConfig;
-export const setConfig = newConfig => { document.appConfig = newConfig; };
-export const resetConfig = () => { document.appConfig = deepCopy(originalConfig.app); };
+export const getConfig = () => document.app.config;
+export const setConfig = newConfig => { document.app.config = newConfig; };
+export const resetConfig = () => { document.app.config = deepCopy(originalConfig.app); };
