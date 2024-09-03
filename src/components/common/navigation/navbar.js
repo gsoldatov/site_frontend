1c6aa36e1f80e12637968760ecd4afe48e98cd26
-- <smoke571reg@yandex.ru>
2024-09-03 13:25:08 +0400

    Updated navigation bar to use normal layout flow, instead of being fixed at the top of the screen

--- src/components/common/navigation/navbar.js
+++ src/components/common/navigation/navbar.js
@@ -1,6 +1,5 @@
-import React, { memo, useMemo, useState, useEffect, useRef } from "react";
+import React, { memo, useMemo, useState } from "react";
 import { Icon, Menu } from "semantic-ui-react";
-import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { OnResizeWrapper } from "../on-resize-wrapper";
@@ -18,15 +17,9 @@ import StyleNavigation from "../../../styles/navigation.css";
  * Navigation bar top-level component.
  */
 export const Navbar = memo(() => {
-    const placeholderRef = useRef();
-    const location = useLocation();
-
     // Stacked menu expand/collapse control display
     const [isStacked, setIsStacked] = useState(window.innerWidth < 768);    // SUIR @media threshold
     const [isExpanded, setIsExpanded] = useState(false);
-
-    // Navbar placeholder height
-    const [placeholderHeight, setPlaceholderHeight] = useState(0);
 
     // Resize callback
     const onResizeCallback = useMemo(() => navbar => {
@@ -36,25 +29,10 @@ export const Navbar = memo(() => {
             const width = parseInt(computedStyle.width.replace("px", ""));
             const newIsStacked = width < 768;   // SUIR @media threshold
             setIsStacked(newIsStacked);
-
-            // Update placeholder height
-            const newNavbarHeight = parseInt(computedStyle.height.replace("px", ""));
-            if (placeholderHeight !== newNavbarHeight) setPlaceholderHeight(newNavbarHeight);
         }
-    }, [isStacked, placeholderHeight]);
-
-    // Update placeholder height after first render
-    useEffect(() => {
-        if (isStacked && placeholderRef.current) {
-            const navbar = placeholderRef.current.parentNode.querySelector(".navigation-bar");
-            const newNavbarHeight = parseInt(getComputedStyle(navbar).height.replace("px", ""));
-            if (placeholderHeight !== newNavbarHeight) setPlaceholderHeight(newNavbarHeight);
-        }
-    }, []);
+    }, [isStacked]);
 
     // CSS classnames
-    const placeholderClassName = "navigation-bar-placeholder" + (isStacked ? " is-stacked" : "");
-    const placeholerStyle = { height: placeholderHeight };
     const mainMenuClassname = "navigation-bar" + (isStacked ? " is-stacked" : "");
 
     // Menu items props
@@ -66,37 +44,30 @@ export const Navbar = memo(() => {
 
     const editedObjectsLabelText = useSelector(state => Object.keys(state.editedObjects).length || null);
     
-    /* 
-        Menu position is fixed to take 100% of screen width, `navigation-bar-placeholder` takes menu's positon in the flow
-
+    /*
         `vertical` <Menu> prop is used instead of `stackable` prop of <Menu> to avoid inconsistency when when displaying composite object page
         (small viewport width forces navbar to be stackable even body width is > 768px) 
     */
     return (
-        <div className="navigation-bar-container">
-            <div ref={placeholderRef} className={placeholderClassName} style={placeholerStyle} />
-            <OnResizeWrapper callback={onResizeCallback}>
-                <Menu fluid vertical={isStacked} size="large" className={mainMenuClassname}
-                    activeIndex={location.pathname}>
-                    
-                    <ExpandToggle isStacked={isStacked} isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-                    
-                    <NavbarItem isDisplayed={menuItemsAreVisible} text="Index" url="/" />
+        <OnResizeWrapper callback={onResizeCallback}>
+            <Menu fluid vertical={isStacked} size="large" className={mainMenuClassname} activeIndex={location.pathname}>
+                <ExpandToggle isStacked={isStacked} isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+                
+                <NavbarItem isDisplayed={menuItemsAreVisible} text="Index" url="/" />
 
-                    <NavbarSubmenu isDisplayed={loggedInAndVisible} isStacked={isStacked} text="Objects">
-                        <NavbarSubmenuItem text="New" url="/objects/edit/new" isStacked={isStacked} />
-                        <NavbarSubmenuItem text="List" url="/objects/list" isStacked={isStacked} />
-                        <NavbarSubmenuItem text="Edited Objects" url="/objects/edited" labelText={editedObjectsLabelText} labelColor="green" isStacked={isStacked} />
-                    </NavbarSubmenu>
-                    
-                    <NavbarItem isDisplayed={menuItemsAreVisible} text="Tags" url="/tags/list" />
-                    <NavbarItem isDisplayed={loggedInAsAdminAndVisible} text="Admin Page" url="/admin" />
+                <NavbarSubmenu isDisplayed={loggedInAndVisible} isStacked={isStacked} text="Objects">
+                    <NavbarSubmenuItem text="New" url="/objects/edit/new" isStacked={isStacked} />
+                    <NavbarSubmenuItem text="List" url="/objects/list" isStacked={isStacked} />
+                    <NavbarSubmenuItem text="Edited Objects" url="/objects/edited" labelText={editedObjectsLabelText} labelColor="green" isStacked={isStacked} />
+                </NavbarSubmenu>
+                
+                <NavbarItem isDisplayed={menuItemsAreVisible} text="Tags" url="/tags/list" />
+                <NavbarItem isDisplayed={loggedInAsAdminAndVisible} text="Admin Page" url="/admin" />
 
-                    <NavbarSearch isStacked={isStacked} />
-                    <NavbarSecondaryMenu isStacked={isStacked} isExpanded={isExpanded} />
-                </Menu>
-            </OnResizeWrapper>
-        </div>
+                <NavbarSearch isStacked={isStacked} />
+                <NavbarSecondaryMenu isStacked={isStacked} isExpanded={isExpanded} />
+            </Menu>
+        </OnResizeWrapper>
     );
 });
 
