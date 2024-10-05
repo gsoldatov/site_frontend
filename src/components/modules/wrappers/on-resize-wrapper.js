0e47e2e3ad65f8c9d330700068a84d0a654fd1ea
-- <smoke571reg@yandex.ru>
2024-10-05 16:58:52 +0300

    Updated <OnResizeWrapper> comments

--- src/components/modules/wrappers/on-resize-wrapper.js
+++ src/components/modules/wrappers/on-resize-wrapper.js
@@ -7,7 +7,10 @@ import { enumDebounceDelayRefreshMode } from "../../../util/enum-debounce-delay-
 
 
 /**
- * Wrapper component, which fires (with debouncing) provided `callback` when resize of its child occures and passes computed style into it.
+ * Subscribes to resize events of the DOM element rendered by a child component.
+ * Fires debounced `callback` function and provides reference to the DOM element to it.
+ * 
+ * NOTE: component supports only a single child component, which renders a DOM element.
  */
 export const OnResizeWrapper = ({ callback, children, delay = 100 }) => {
     const innerRef = useRef();
@@ -18,10 +21,11 @@ export const OnResizeWrapper = ({ callback, children, delay = 100 }) => {
         if (innerRef.current) abortResizeCallbackRef.current = callback(innerRef.current);
     }, delay, enumDebounceDelayRefreshMode.noRefresh), [callback]);
 
-    // Run `callback` when it's changed and initialize a ResizeObserver object (if it's supported), which will trigger onResize function
-    // `useLayoutEffect` is used instead of `useEffect` in order to run callback synchronously
+    // Handle subscription to resize events of the child element.
+    // `useLayoutEffect` is used instead of `useEffect` in order to run callback synchronously.
     useLayoutEffect(() => {
-        // onResize();
+        // Add a ResizeObserver for the child
+        // (this also runs the `callback` before element is painted: https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver#observation_errors)
         if (window.ResizeObserver) {
             resizeObserver.current = new ResizeObserver(onResize);
             resizeObserver.current.observe(innerRef.current);
