3fd8d2fca00b3aaf1764db959b2e2ec9c07d099e
-- <smoke571reg@yandex.ru>
2024-10-13 15:54:30 +0300

    `Locator` class now filters matching nodes by distance to parent; fixed`refresh` method to be callable when parent locator does not match to a single node; fixed binding `this` to methods

--- tests/_page-object-models/_util/locator.js
+++ tests/_page-object-models/_util/locator.js
@@ -4,17 +4,27 @@ import { waitFor } from "@testing-library/react";
 /**
  * Helper class for common operations with DOM nodes.
  * 
- * Accepts a CSS `selector` and an optional `parent` object, which can contain a reference to a parent `node` or `locator`.
+ * Accepts:
+ * - a CSS `selector`;
+ * - `parent` object, which can contain a reference to a parent `node` or `locator` (if omitted, container will be set to `document.body`);
+ * - `options` object with additional locator configuration options:
+ *      - `minDepth`: min distance between node & its parent in DOM tree (defaults to 1);
+ *      - `maxDepth`: max distance between node & its parent in DOM tree (defaults to 100);
  */
 export class Locator {
-    constructor(selector, parent = {}) {
+    constructor(selector, parent = {}, options = {}) {
+        // Selector & parent node or locator
         this.selector = selector;
         this.parentNode = parent.node;
         this.parentLocator = parent.locator;
 
-        if(this.parentLocator) this.parentLocator.childLocators.push(this);
+        // Options
+        this.minDepth = "minDepth" in options ? options.minDepth : 1;
+        this.maxDepth = "maxDepth" in options ? options.maxDepth : 100;
 
+        if(this.parentLocator) this.parentLocator.childLocators.push(this);
         this.childLocators = [];
+
         this.macthingNodes = null;
     }
 
@@ -22,13 +32,31 @@ export class Locator {
      * Updates macthing nodes for this locator and its children.
      */
     refresh() {
-        const parentNode = this.parentNode ? this.parentNode :
-                           this.parentLocator ? this.parentLocator.node : document.body;
+        try {
+            // Try to get parent node (might fail, if `parentLocator` can't find the parent node)
+            const parentNode = this.parentNode ? this.parentNode :
+                            this.parentLocator ? this.parentLocator.node : document.body;
+            
+            // Find all matching nodes & filter them by min & max depths
+            let macthingNodes = parentNode.querySelectorAll(this.selector);
+            if (macthingNodes !== null) macthingNodes = [...macthingNodes].filter(node => {
+                let depth = 0, curr = node;
+                while (curr !== parentNode) {
+                    curr = curr.parentNode;
+                    depth += 1;
+                }
+                return depth >= this.minDepth && depth <= this.maxDepth;
+            });
 
-        if (parentNode) this.macthingNodes = parentNode.querySelectorAll(this.selector);
-        else  this.macthingNodes = null;
+            this.macthingNodes = macthingNodes;
+        } catch (e) {
+            // If`SingleNodeNotFound` is thrown, parent node can't be accessed via parent locator,
+            // which means that current locator can't find matches
+            if (e instanceof SingleNodeNotFound) this.macthingNodes = null;
+            else throw e;
+        }
 
-        // Refresh, if a single node if found, otherwise reset to null
+        // Refresh child locators, if a single node if found, otherwise reset to null
         if(this.macthingNodes !== null && this.macthingNodes.length === 1) this.childLocators.forEach(child => { child.refresh() });
         else this.childLocators.forEach(child => { child._resetMatchingNodes() });
     }
@@ -46,7 +74,9 @@ export class Locator {
      */
     get node() {
         if (this.macthingNodes === null) this.refresh();
-        if (this.macthingNodes.length !== 1) throw Error(`Can't access a node via locator: found ${this.macthingNodes.length} matches for selector '${this.selector}'.`);
+        const macthingNodesLength = this.macthingNodes === null ? 0 : this.macthingNodes.length;
+        if (macthingNodesLength  !== 1)
+            throw new SingleNodeNotFound(`Can't access a node via locator: found ${macthingNodesLength} matches for selector '${this.selector}'.`);
         return this.macthingNodes[0];
     }
 
@@ -56,14 +86,14 @@ export class Locator {
      */
     isOnPage(refresh) {
         if (refresh || this.macthingNodes === null) this.refresh();
-        return this.macthingNodes.length > 0 && document.body.contains(this.macthingNodes[0]);
+        return this.macthingNodes !== null && this.macthingNodes.length > 0 && document.body.contains(this.macthingNodes[0]);
     }
 
     /**
      * Tries to wait for a matching node to appear on the page.
      */
     async waitFor() {
-        await waitFor((() => {
+        await waitFor((function() {
             expect(this.isOnPage(true)).toBeTruthy();
         }).bind(this));
     }
@@ -72,7 +102,7 @@ export class Locator {
      * Tries to wait for matching nodes to disappear from the page.
      */
     async waitForAbsence() {
-        await waitFor((() => {
+        await waitFor((function() {
             expect(this.isOnPage(true)).toBeFalsy();
         }).bind(this));
     }
@@ -85,3 +115,6 @@ export class Locator {
         this.childLocators.forEach(child => { child._resetMatchingNodes() });
     }
 }
+
+
+class SingleNodeNotFound extends Error {};
