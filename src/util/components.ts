ab4d9504beaaa24dd8b7c6cb365384c7a2bb56f2
-- <smoke571reg@yandex.ru>
2024-11-03 00:58:54 +0300

    Added typing for components & size context wrapper utility functions

--- src/util/components.ts
+++ src/util/components.ts
@@ -1,4 +1,4 @@
-import { Children, isValidElement } from "react";
+import { type ReactNode, Children, isValidElement } from "react";
 import { createStore } from "redux";
 
 
@@ -9,13 +9,14 @@ import { createStore } from "redux";
  * NOTE: `child.type(child.props)` does not work for some SUIR components due to props being read-only.
  * A possible workaround is to add a <div> wrapper in such cases.
  */
-export const anyChildIsRendered = children => {
+export const anyChildIsRendered = (children: ReactNode) => {
     const childArray = Children.toArray(children);
 
     if (childArray.length === 0) return false;
     return childArray.some(child => {
         if (!isValidElement(child)) return false;
         // child.type can be at least a function or a string
+        // @ts-ignore (child.type is not always callable)
         if (typeof(child.type) === "function" && !child.type(child.props)) return false;
         return child.type;
     });
@@ -27,9 +28,9 @@ export const anyChildIsRendered = children => {
  * 
  * To use created store, context, provider component & hook must be set up, as described here: https://react-redux.js.org/api/hooks#custom-context
  */
-export const createComponentStore = (initialState) => {
+export const createComponentStore = <T extends Record<string, any>>(initialState: T) => {
     // A shortened reducer, which does not use action types and simply returns a new version of `state` with new values from `stateProps`.
-    const reducer = (state, stateProps) => {
+    const reducer = (state: T, stateProps: Partial<T>) => {
         // Validate function params & new props' names
         if (typeof(stateProps) !== "object" || stateProps === null) throw Error(`Received ${stateProps} instead of an object.`);
         const statePropsKeys = [...Object.keys(stateProps)].filter(key => key !== "type");
@@ -37,11 +38,12 @@ export const createComponentStore = (initialState) => {
         if (nonExistingProps.length > 0) throw Error(`Received props, which do not exist in state: ${nonExistingProps}.`);
 
         // Don't update state, if no new values were provided
-        if (!statePropsKeys.some(key => statePropsKeys[key] !== state[key])) return state;
+        if (!statePropsKeys.some(key => stateProps[key] !== state[key])) return state;
 
         // Return updated state
         return { ...state, ...stateProps };
     };
 
+    // @ts-ignore (reducer type does not match the type required by Redux, but it still works)
     return createStore(reducer, initialState);
 };
--- src/util/size-context-wrappers.ts
+++ src/util/size-context-wrappers.ts
@@ -4,7 +4,7 @@
  * @param {number[]} thresholds
  * @returns {number}
  */
-export const getThresholdForValue = (value, thresholds) => {
+export const getThresholdForValue = (value: number, thresholds: number[]) => {
     for (let i = 0; i < thresholds.length; i++)
         if (value <= thresholds[i]) return i;
 
