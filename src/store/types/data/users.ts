97c6fcec9198e58c7321fe53bf0e2b0dbd0f892a
-- <smoke571reg@yandex.ru>
2024-11-03 16:46:33 +0300

    Added state.users types

---
+++ src/store/types/data/users.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { positiveInt } from "../../../util/types/common";
+import { UserLevels } from "./auth";
+
+
+const userMin = z.object({
+    user_id: positiveInt,
+    registered_at: z.string().datetime({ offset: true }),
+    username: z.string()
+});
+
+const registeredUserLevels = Object.keys(UserLevels).filter(k => isNaN(k as any) && k != "anonymous") as [string, ...string[]];
+export const userFull = userMin.extend({
+    user_level: z.enum(registeredUserLevels),
+    can_login: z.boolean(),
+    can_edit_objects: z.boolean()
+});
+
+export const user = userMin.or(userFull);
