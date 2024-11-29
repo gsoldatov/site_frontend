d52c940a18c6c0fb3d24a0ecfdbee19fa9b33f33
-- <smoke571reg@yandex.ru>
2024-11-29 12:09:26 +0300

    Added typing for `updateSubobjectsOnSave` composite update command

--- src/reducers/data/edited-objects.ts
+++ src/reducers/data/edited-objects.ts
@@ -4,7 +4,6 @@ import { TagsSelectors } from "../../store/selectors/data/tags";
 import { TagsTransformer } from "../../store/transformers/data/tags";
 
 import { getUpdatedToDoList, type ToDoListUpdateParams } from "../../store/updaters/data/to-do-lists";
-import { getStateWithCompositeUpdate as OLD_getStateWithCompositeUpdate } from "../helpers/object-composite";
 import { EditedCompositeUpdaters, type GetUpdatedEditedCompositeParams } from "../../store/updaters/data/edited-composite";
 
 import type { State } from "../../store/types/state";
@@ -142,10 +141,7 @@ export const updateEditedComposite = (objectID: number, update: GetUpdatedEdited
 
 const _updateEditedComposite = (state: State, action: {objectID: number, update: GetUpdatedEditedCompositeParams }): State => {
     const { objectID, update } = action;
-    if (["addNewSubobject", "addExistingSubobject", "updateSubobject", "toggleSubobjectsIsPublished",
-        "setSubobjectsFetchError", "updatePositionsOnDrop", 
-    ].includes(update.command)) return EditedCompositeUpdaters.runUpdateCommand(state, objectID, update);
-    return OLD_getStateWithCompositeUpdate(state, objectID, update);    // TODO remove old function call
+    return EditedCompositeUpdaters.runUpdateCommand(state, objectID, update);
 };
 
 
--- src/store/updaters/data/edited-composite.ts
+++ src/store/updaters/data/edited-composite.ts
@@ -1,13 +1,19 @@
 import { deepCopy } from "../../../util/copy";
 
+import { EditedObjectsSelectors } from "../../selectors/data/objects/edited-objects";
 import { EditedObjectsUpdaters } from "./edited-objects";
+import { ObjectsUpdaters } from "./objects";
 import { CompositeSelectors } from "../../selectors/data/objects/composite";
 import { getStateWithResetEditedObjects, getStateWithDeletedEditedNewSubobjects } from "../../../reducers/helpers/object";
 
-import { compositeSubobject, getCompositeSubobject, type CompositeSubobject } from "../../types/data/composite";
+import {
+    compositeSubobject, getCompositeSubobject, SubobjectDeleteMode, 
+    type CompositeSubobject, type CompositeSubobjects
+} from "../../types/data/composite";
 import { type EditedObjects } from "../../types/data/edited-objects";
 import type { State } from "../../types/state"
 import { type ObjectsAddRequestObjectData, type ObjectsAddResponseBodyObject } from "../../../fetches/types/data/objects/add";
+import { type ObjectsTags } from "../../types/data/objects-tags";
 
 
 /** Contains methods, which update state of an edited composite object & related data (edited subobjects, etc). */
@@ -279,8 +285,123 @@ const updatePositionsOnDrop = (state: State, objectID: number, update: ParamsUpd
 };
 
 
+/**
+ * Updates state after add or update of a composite object:
+ * - removes any new subobjects from state.editedObjects if the main object is not composite (for when it was changed after subobject creation);
+ * - removes deleted and fully deleted existing subobjects from state;
+ * - updates new & modified subobjects in state.editedObjects (maps subobject IDs & created_at & modified_at timestamps);
+ * - adds new & modified subobject attributes & data to state storages;
+ * - adds empty records in objectsTags storage for new subobjects;
+ * - maps subobject IDs of the saved composite object and removes deleted subobjects in state.editedObjects[objectID];
+ * - updates subobject row positions in state.editedObjects[objectID].
+ * 
+ * Object attributes, tags & data of the saved object are added to the storages in the add/update fetch functions.
+ */
 const updateSubobjectsOnSave = (state: State, objectID: number, update: ParamsUpdateSubobjectsOnSave): State => {
-    throw Error("Not implemented"); // TODO add typed post-update composite data typing or remove
+    // Don't update non-existing edited object (e.g. when updating data via /objects/view/:id fetches)
+    if (state.editedObjects[objectID] === undefined) return state;
+
+    // `object` contains response object attributes & data, `object_data` contains object data as it was sent in request
+    const { object, object_data } = update;
+
+    // If object is not composite, delete any new subobjects which were created before object type was changed
+    if (object.object_type !== "composite") return getStateWithDeletedEditedNewSubobjects(state, [objectID]);
+    let newState = state;
+
+    // Remove new and unchanged (non-fully) deleted existing objects from state
+    let subobjectIDs = Object.keys(newState.editedObjects[objectID].composite.subobjects).map(id => parseInt(id));
+    const deletedSubobjectIDs = subobjectIDs.filter(subobjectID => 
+                                newState.editedObjects[objectID].composite.subobjects[subobjectID].deleteMode === SubobjectDeleteMode.subobjectOnly
+                                && EditedObjectsSelectors.isNewOrUnchangedExisting(newState, subobjectID));
+    newState = ObjectsUpdaters.deleteObjects(newState, deletedSubobjectIDs);
+    
+    // Remove fully deleted existing objectsfrom state
+    let fullyDeletedExistingSubobjectIDs = subobjectIDs.filter(subobjectID => {
+        return subobjectID > 0 && newState.editedObjects[objectID].composite.subobjects[subobjectID].deleteMode === SubobjectDeleteMode.full
+    });
+    newState = ObjectsUpdaters.deleteObjects(newState, fullyDeletedExistingSubobjectIDs);
+
+    // Map new subobject IDs in state.editedObjects and update object_id, created_at & modified_at values
+    // Also add empty records in objectsTags storage for new subobjects
+    const objectUpdateTimeStamp = object.modified_at;
+    const IDMapping = object.object_data!.id_mapping;
+    if (!("subobjects" in object_data)) throw Error("Missing `subobjects` in object_data.");
+    const modifiedExistingSubobjectIDs = object_data.subobjects.filter(so => so.object_id > 0 && "object_name" in so).map(so => so.object_id);
+    let newEditedObjects: EditedObjects = {}, newObjectsTags: ObjectsTags = {};
+    for (let oldObjectID of Object.keys(newState.editedObjects).map(id => parseInt(id))) {
+        const isNewSubobject = IDMapping[oldObjectID] !== undefined;
+        const isModifiedExistingSubobject = modifiedExistingSubobjectIDs.indexOf(oldObjectID) > -1;
+
+        // If edited object was a new subobject, copy it, update its ID and add created_at & modified_at values
+        // Add an empty list as its current tags
+        if (isNewSubobject) {
+            const newObjectID = IDMapping[oldObjectID];
+            newEditedObjects[newObjectID] = { ...newState.editedObjects[oldObjectID] };
+            newEditedObjects[newObjectID].object_id = newObjectID;
+            newEditedObjects[newObjectID].created_at = objectUpdateTimeStamp;
+            newEditedObjects[newObjectID].modified_at = objectUpdateTimeStamp;
+            newObjectsTags[newObjectID] = [];
+        }
+        // If edited object was an existing modified subobject, copy it and update its modified_at value
+        else if (isModifiedExistingSubobject) {
+            newEditedObjects[oldObjectID] = { ...newState.editedObjects[oldObjectID] };
+            newEditedObjects[oldObjectID].modified_at = objectUpdateTimeStamp;
+        }
+        // If edited object was not updated, do not copy it
+        else {
+            newEditedObjects[oldObjectID] = newState.editedObjects[oldObjectID];
+        }
+    }
+    newState = { ...newState, editedObjects: newEditedObjects, objectsTags: { ...state.objectsTags, ...newObjectsTags } };
+
+    // Filter new & modified existing subobjects and map new subobject IDs from object_data
+    let subobjectsToAddToState = object_data.subobjects.filter(so => "object_name" in so).map(so => {
+        const newSO = deepCopy(so);
+        const newObjectID = IDMapping[so.object_id];
+        if (newObjectID !== undefined) {
+            newSO.object_id = newObjectID;
+            newSO.created_at = objectUpdateTimeStamp;
+            newSO.modified_at = objectUpdateTimeStamp;
+        } else {
+            newSO.created_at = newState.objects[so.object_id].created_at;
+            newSO.modified_at = objectUpdateTimeStamp;
+        }
+        return newSO;
+    });
+
+    // Add new & modified existing attributes & data to state
+    newState = ObjectsUpdaters.addObjectsAttributes(newState, subobjectsToAddToState);
+    newState = ObjectsUpdaters.addObjectsDataFromBackend(newState, subobjectsToAddToState);
+    
+    // Map subobjectIDs of the composite object and remove deleted subobjects in state.editedObjects[objectID].
+    // Update row positions of non-deleted objects.
+    const newRowPositions: Record<number, number> = {};
+    object_data.subobjects.forEach(so => {
+        newRowPositions[so.object_id] = so.row;
+    });
+    const newComposite = { ...newState.editedObjects[objectID].composite };
+    const newSubobjects: CompositeSubobjects["subobjects"] = {};
+    Object.keys(newComposite.subobjects).map(id => parseInt(id)).forEach(subobjectID => {
+        // Filter out deleted subobjects
+        if (deletedSubobjectIDs.indexOf(subobjectID) > -1 || fullyDeletedExistingSubobjectIDs.indexOf(subobjectID) > -1) return;
+
+        const mappedSubobjectID = subobjectID in IDMapping ? IDMapping[subobjectID] : subobjectID;
+        newSubobjects[mappedSubobjectID] = { ...newComposite.subobjects[subobjectID], row: newRowPositions[subobjectID] };
+    });
+    newComposite.subobjects = newSubobjects;
+
+    newState = {
+        ...newState,
+        editedObjects: {
+            ...newState.editedObjects,
+            [objectID]: {
+                ...newState.editedObjects[objectID],
+                composite: newComposite
+            }
+        }
+    };
+
+    return newState;
 };
 
 
