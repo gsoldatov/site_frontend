f18c3a8b3ac6b822b9ece46d0a70bf88982dfecc
-- <smoke571reg@yandex.ru>
2025-12-02 18:14:58 +0300

    Added typing for <DisplayControlCheckbox>

--- src/components/modules/edit/display/display-control-checkbox.tsx
+++ src/components/modules/edit/display/display-control-checkbox.tsx
@@ -4,12 +4,22 @@ import { Checkbox } from "semantic-ui-react";
 import StyleDisplayControls from "../../../../styles/modules/edit/display-controls.css";
 
 
+interface DisplayControlCheckboxProps {
+    checked: boolean,
+    indeterminate?: boolean,
+    onClick: (e: React.MouseEvent) => void,
+    label: string
+};
+
+
 /**
  * Display control checkbox component.
  * 
  * Implements UI logic only. Specific props should be provided by a wrapper component.
  */
-export const DisplayControlCheckbox = memo(({ checked, indeterminate, onClick, label }) => {
+export const DisplayControlCheckbox = memo(({ checked, indeterminate, onClick, label }: DisplayControlCheckboxProps) => {
+    indeterminate = indeterminate || false;
+    
     return (
         <div className="display-control-container">
             <Checkbox className="display-control-checkbox-container" 
